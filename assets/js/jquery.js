56603241656fecb8df2179e684e6ea1846a77e53
Fabiola Palominos <fpalominos3640@e-camp.cl>
2021-07-10 23:36:01 -0400

    actualización del footer

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,3 +1,23 @@
 $(function()  {
   $('[data-bs-toggle="tooltip"]').tooltip();
    });
+
+   (document).ready(function() {
+        $("a").on('click', function(event) {
+          behavior
+          if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 400, function() {
+
+              window.location.hash = hash;
+            });
+          }
+        });
+      });
